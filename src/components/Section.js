b5fc2a409e49a1e5ba02b6a03878bc4442030e08
Maksym <mvsozykin@gmail.com>
2024-10-04 21:11:23 +0300

    my first react app

---
+++ src/components/Section.js
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+
+export default function Section({ title, children }) {
+  return (
+    <div>
+      {title && <h2>{title}</h2>}
+      {children}
+    </div>
+  );
+}
+
+Section.propTypes = { title: PropTypes.string };
